82660fe1c254a22b65ff7bbef2c7750e0085d267
Singla17 <ssingla2018@gmail.com>
2022-04-21 14:52:06 +0530

    Bin reader updated

--- data_collection/bin_reader.py
+++ data_collection/bin_reader.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 This script converts .bin file data into a Sequence Radar data cubes in the format of numpy array
-The output numpy array is of the shape = [Number_of_frames x Number_of_Virtual_Antenna x Number_of_adc_sample_per_chirp x Number_of_chirps]
+The output numpy array is of the shape = [Number_of_frames x Number_of_chirps x Num of Rx x Number_of_adc_samples_per_chirp]
 can be called using the format "python bin_reader --input_path=<Path to .bin file> --json_path=<Path to JSON file containing params> --save_path=<Path to save the npy format of data to>"
 
 @author: Somanshu
+
+Our raw data is collected using IWR6843 With DCA1000 [see: https://www.ti.com/lit/an/swra581b/swra581b.pdf?ts=1648804249448&ref_url=https%253A%252F%252Fwww.google.com%252F]
+We assume complex data collection.
 """
 import argparse
 import json
@@ -26,6 +29,17 @@ def getparams(json_file_path):
     """
     Input: Path of the json file which stores the parameters.
     Output: Returns all the Radar parameters required for reading .bin file.
+    
+    Param Meaning:
+        Num of Frames: Number of frames in the gesture
+        Num of chirp per frame: Number of chirp in a single frame 
+        Num adc sample per chirp: Number of ADC samples per chirp
+        Num range bin: Equal to Num adc sample per chirp
+        Num virtual antennas: Num Tx * Num Rx antennas
+        chirp config per chirp: Number of Tx antenna 
+        [basically number of chirp configurations outputted from the device per frame]
+        num real channel: Rx ifd real data capture , 2*Rx if complex data capture
+        total sample per frame: As per TI manual.    
     """
     file = open(json_file_path)
     file = json.load(file)
@@ -36,7 +50,7 @@ def getparams(json_file_path):
     num_virtual_antennas= file["Number of virtual antenna"]
     chirp_config_per_chirp= file["ChirpConfigs per chirp"]
     num_real_channel= file["Number of real channels "]
-    total_sample_per_frame = num_real_channel*num_adc_sample_per_chirp*chirp_config_per_chirp*num_of_chirp_per_frame
+    total_sample_per_frame = num_real_channel*num_adc_sample_per_chirp*chirp_config_per_chirp*num_of_chirp_per_frame ## see data format TI slide to understand.
     return num_of_frames, num_of_chirp_per_frame, num_adc_sample_per_chirp, num_range_bin, num_virtual_antennas, chirp_config_per_chirp, num_real_channel, total_sample_per_frame
     
 
@@ -45,23 +59,22 @@ def read_bin_file (path, num_of_frames, num_of_chirp_per_frame, num_adc_sample_p
     Given all the RADAR parameters and the path of the input .bin file returns the data in numpy format
     """
     data_file = np.fromfile(path , dtype=np.int16)
-    data = data_file.reshape (num_of_frames ,(total_sample_per_frame//4) ,4)
-    rearrange_data = np.copy(data)
+    data = data_file.reshape (num_of_frames,num_of_chirp_per_frame,num_real_channel//2,num_adc_sample_per_chirp*2)
+    final_data = np.zeros((num_of_frames,num_of_chirp_per_frame,num_real_channel//2,num_adc_sample_per_chirp))
     
-    #convert the data to Rx0I0 , Rx0Q0 , Rx0I1 , Rx0Q1 , . . .
-    rearrange_data [ : , : , 2 ] = data [ : , : , 1 ]
-    rearrange_data [ : , : , 1 ] = data [ : , : , 2 ]
-    del(data)
-    rearrange_data = rearrange_data.reshape(num_of_frames, 2*num_adc_sample_per_chirp, (num_real_channel//2) * chirp_config_per_chirp, num_of_chirp_per_frame)
-    rearrange_data = np.swapaxes(rearrange_data , 1 , 3 )
     
-    #Convert to Complex no
-    radar_data_real = rearrange_data [ : , : : 2 , : , : ]
-    radar_data_imag = rearrange_data [ : , 1 : : 2 , : , : ]
-    radar_data = 1j*radar_data_imag
-    radar_data += radar_data_real
-    radar_data = np.moveaxis (radar_data , [0,1,2,3] , [0,2,1,3] )
-    return radar_data
+    for i in range(num_of_frames):
+        for j in range(num_of_chirp_per_frame):
+            for k in range(num_real_channel//2):
+                for l in range(num_adc_sample_per_chirp):
+                    
+                    if l % 2 ==0:
+                        final_data[i][j][k][l] = data[i][j][k][2*l] + i*data[i][j][k][2*l+2]
+                    else:
+                        final_data[i][j][k][l] = data[i][j][k][2*l-1] + i*data[i][j][k][2*l+1]
+    
+    
+    return final_data
 
 if __name__ == "__main__":
     args = parse_args()
